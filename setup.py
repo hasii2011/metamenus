4d17d8342dcf43a702efc1c8601a897558a7fbda
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-11-21 13:13:22 -0700

    Prep to push to PyPi;  Now push to prod

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="metamenus",
-    version="0.14.3",
+    version="0.14.4",
     author='E.A. Tacao',
     author_email='mailto@tacao.com.br',
     maintainer='Humberto Sanchez II',
     maintainer_email='humberto.a.sanchez.ii@gmail.com',
-    description='wxPython Menu Maker',
+    description='Declarative Menu Maker for wxPython',
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/hasii2011/metamenus",
